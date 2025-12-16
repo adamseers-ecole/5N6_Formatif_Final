2c3fb52c05d569f6e193249856ce79a5d040d0f9
Adam Seers <131391702+adamseers-ecole@users.noreply.github.com>
2025-12-16 10:00:31 -0500

    FCT Partie requêtes et question

--- requetes/firebase_start_final/lib/main.dart
+++ requetes/firebase_start_final/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -37,6 +38,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  String resultat = "";
+  final Dio dio = Dio();
+  final _dividende = TextEditingController(text: "100");
+  final _diviseur = TextEditingController(text: "20");
+
+  Future<void> _requete() async{
+    try {
+      final response = await dio.get("https://examen-final-a24.azurewebsites.net/Exam2024/Division/${_dividende.text}/${_diviseur.text}");
+      setState(() {
+        resultat = response.data['resultat'].toString();
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Réponse : " +resultat)));
+
+    } on DioException catch(e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Ça marche pas nom ami: "+ e.message.toString())));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,8 +65,29 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("Allo"),
+        child: Column(
+          children: [
+            TextField(controller: _dividende,),
+            TextField(controller: _diviseur,),
+            ElevatedButton(
+              onPressed: () async {
+                await _requete();
+              },
+              child: Text("Calculer"),
+            ),
+            Text("Réponse : " +resultat),
+          ],
+        ),
       ),
     );
   }
 }
+//Qu'est-ce que le contrôle d'accès, et comment est-il mis en place dans le TP3 du cours d'applications mobiles avancées?
+/*
+
+Le contrôle d'accès dans le TP3 permet de garder les informations des utilisateurs sécuritaire
+et de ne laisser que eux y accéder. Nous pouvons le mettre en place en allant sur firebase,
+ensuite dans la base de données et finalement dans les règles de la base de données et faire en sorte
+que le uid du l'utilisateur qui fait la requête corresponde au uid de la collection dans la base de données.
+
+ */

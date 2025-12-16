3a1e614560fcac696cfd760384c910d2995ed460
Adam Seers <131391702+adamseers-ecole@users.noreply.github.com>
2025-12-16 09:07:41 -0500

    FCT Firestore complété

--- firebase/firebase_start_final/lib/main.dart
+++ firebase/firebase_start_final/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'Etudiant.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
@@ -37,6 +39,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Etudiant student = new Etudiant(nom: "", prenom: "", complete: false);
+
+  Future<void> _obtenir() async {
+    final DocumentSnapshot doc = await FirebaseFirestore.instance.collection('etudiants').doc('2128368').get();
+
+    this.student = Etudiant.fromJson(doc.data() as Map<String, dynamic>, doc.id);
+  }
+  Future<void> _update() async{
+    await FirebaseFirestore.instance.collection('etudiants').doc('2128368').update({'complete': !student.complete});
+    _obtenir();
+
+    setState(() {
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +62,31 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text("Allo"),
+        child: Column(
+          children: [
+            ElevatedButton(
+                child: Text("Obtenir"),
+                onPressed: () async {
+                  await _obtenir;
+                  setState(() {
+
+                  });
+                }
+            ),
+            Text(student.nom ?? "pas trouvé"),
+            Text(student.prenom ?? "pas trouvé"),
+            ElevatedButton(
+                child: Text("Mettre à jour"),
+                onPressed: () async {
+                  await _update();
+                  setState(() {
+
+                  });
+                }
+            ),
+            Text(student.complete.toString() ?? "false"),
+          ],
+        ),
       ),
     );
   }
